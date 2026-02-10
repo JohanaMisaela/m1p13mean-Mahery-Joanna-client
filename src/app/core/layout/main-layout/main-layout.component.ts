8fababb67e55d1cc2a9b80efac3bb289f4fad0ef
JohanaMisaela <johanna.tahianaharison@gmail.com>
2026-02-10 20:50:06 +0300

    feat: add and integrate footer component into the main layout.

--- src/app/core/layout/main-layout/main-layout.component.ts
+++ src/app/core/layout/main-layout/main-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
     selector: 'app-main-layout',
-    imports: [HeaderComponent, RouterOutlet],
+    standalone: true,
+    imports: [HeaderComponent, RouterOutlet, FooterComponent],
     templateUrl: './main-layout.component.html',
     styles: []
 })
